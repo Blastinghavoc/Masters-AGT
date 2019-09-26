b18832d2da9791e7dd2e364906279aeec24ec434
Jacob Taylor <taylj024@gmail.com>
2019-09-26 15:35:03 +0100

    Initial commit before starting lab 1

---
+++ AGT_LAB01.Demo/engine/src/engine/renderer/renderer.cpp
@@ -0,0 +1,87 @@
+#include "pch.h"
+#include "renderer.h"
+#include "platform/opengl/gl_shader.h"
+
+engine::renderer::scene_data* engine::renderer::s_scene_data = new scene_data;
+engine::renderer* engine::renderer::s_instance = new renderer();
+
+void engine::renderer::init()
+{
+    s_instance->m_shader_library = std::make_unique<shader_library>();
+    renderer_api::init();
+
+    renderer::shaders_library()->load("assets/shaders/mesh_static.glsl");
+    //renderer::shaders_library()->load("assets/shaders/mesh_animated.glsl");
+	renderer::shaders_library()->load("assets/shaders/mesh_lighting.glsl");
+	renderer::shaders_library()->load("assets/shaders/colour.glsl");
+}
+
+void engine::renderer::begin_scene(camera& camera, const ref<shader>& shader)
+{
+    s_scene_data->view_projection_matrix = camera.view_projection_matrix();
+    s_scene_data->shader = shader;
+    shader->bind();
+    std::dynamic_pointer_cast<engine::gl_shader>(shader)->set_uniform("u_view_projection", s_scene_data->view_projection_matrix);
+}
+
+void engine::renderer::end_scene()
+{
+    s_scene_data->shader->unbind(); 
+}
+
+void engine::renderer::submit(
+    const ref<shader>& shader, 
+    const ref<vertex_array>& vertex_array, 
+    const glm::mat4& transform /*= glm::mat4(1.f)*/)
+{
+    std::dynamic_pointer_cast<engine::gl_shader>(shader)->set_uniform("u_transform", transform);
+
+    vertex_array->bind();
+    render_command::submit(vertex_array);
+}
+
+void engine::renderer::submit(
+    const ref<shader>& shader,
+    const ref<mesh>& mesh,
+    const glm::mat4& transform /*= glm::mat4(1.f)*/)
+{
+    submit(shader, mesh->va(), transform);
+}
+
+void engine::renderer::submit(
+    const ref<shader>& shader, 
+	const ref<game_object>& object)
+{
+	glm::mat4 transform = glm::mat4(1.0f);
+	object->transform(transform);
+	object->bind_textures();
+    const bool has_meshes = !object->meshes().empty();
+	if (has_meshes)
+	{
+		auto model_meshes = object->meshes();
+		for (const auto& mesh : model_meshes)
+		{
+			submit(shader, mesh, transform);
+		}
+	}
+}
+
+void engine::renderer::submit(
+	const ref<shader>& shader,
+	const ref<skybox>& skybox,
+	const glm::mat4& transform)
+{
+	std::dynamic_pointer_cast<engine::gl_shader>(shader)->set_uniform("skybox_rendering", true);
+	if (skybox->textures().size() == skybox->meshes().size())
+	{
+		uint32_t i = 0;
+		auto model_meshes = skybox->meshes();
+		for (const auto& mesh : model_meshes)
+		{
+			skybox->textures().at(i)->bind();
+			submit(shader, mesh, transform);
+			i++;
+		}
+	}
+	std::dynamic_pointer_cast<engine::gl_shader>(shader)->set_uniform("skybox_rendering", false);
+}
---
+++ AGT_LAB01.Demo/engine/src/platform/opengl/gl_texture.cpp
@@ -0,0 +1,59 @@
+#include "pch.h"
+#include "gl_texture.h"
+
+#include "stb_image.h"
+#include "glad/glad.h"
+
+engine::gl_texture_2d::gl_texture_2d(const std::string& path)
+    : m_path(path)
+{
+    int width, height, channels;
+    stbi_set_flip_vertically_on_load(true);
+    stbi_uc* data = stbi_load(path.c_str(), &width, &height, &channels, 0);
+    CORE_ASSERT(data, "[gl_texture_2d] Texture not loaded correctly");
+    m_width = width;
+    m_height = height;
+
+    GLenum internal_format = 0, data_format = 0;
+    if(channels == 3)
+    {
+        internal_format = GL_RGB8;
+        data_format = GL_RGB;
+    }
+    else if(channels == 4)
+    {
+        internal_format = GL_RGBA8;
+        data_format = GL_RGBA;
+    }
+
+	glEnable(GL_TEXTURE_2D);
+	glGenTextures(1, &m_id);
+	glBindTexture(GL_TEXTURE_2D, m_id);
+    // allocating memory to gpu to store the texture data
+    const int mipmap_levels = 4;
+    glTexStorage2D(GL_TEXTURE_2D, mipmap_levels, internal_format, m_width, m_height);
+
+    // set texture params 
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    // upload texture to gpu 
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, m_width, m_height, data_format, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+    // freeing allocated image buffer
+    stbi_image_free(data);
+}
+
+engine::gl_texture_2d::~gl_texture_2d()
+{
+    glDeleteTextures(1, &m_id);
+}
+
+void engine::gl_texture_2d::bind(uint32_t slot /*= 0*/) const
+{
+    glBindTextureUnit(slot, m_id);
+}
+
